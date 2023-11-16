bc6a396a6eecc6c38ad72e6921d70ed40c0d2d1b
kartik kumar <gtgkartik@gmail.com>
2023-11-17 03:01:24 +0530

    configured the docker container for next js

---
+++ strapibackend/src/api/aa/routes/aa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * aa router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::aa.aa');
---
+++ strapibackend/src/api/aa/services/aa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * aa service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::aa.aa');
